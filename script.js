2836a0e13f2a66b0f1ccfcddc7e5b8d988014e77
May_Edu <mayck22.03@hotmail.com>
2023-05-19 13:59:11 -0300

    aula 01

---
+++ script.js
@@ -0,0 +1,19 @@
+/*
+$valor = confirm("Vamos juntos aprender a programar em JS?");
+
+if ($valor == true){
+    alert("parabens, vamos lá")
+} else {
+    alert("Opção errada, aperte F5 para recarregar a página")
+    StyleSheet.confirm = #fffff
+}
+
+*/
+
+
+$email = prompt("Digite seu email: " );
+// alert("email " + $email + " foi coletado com sucesso")
+
+if ($email == true) {
+   alert("email " + $email + " foi coletado com sucesso");
+}

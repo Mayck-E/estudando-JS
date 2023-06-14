6e26c99e09894928b65ee591531c22cf0fbba6ba
May_Edu <mayck22.03@hotmail.com>
2023-06-14 15:36:15 -0300

    up

--- 02/js/script.js
+++ 02/js/script.js
@@ -1,17 +1,18 @@
 let fruta = 'fruta'
 
-fruta = prompt('nome da fruta')
+let span = document.getElementById('texto')
+fruta = prompt('O que deseja?')
 
 switch (fruta) {
-    case 'maça':
-        alert("Não vendemos essa fruta aqui")
+    case 'maçã':
+        alert('Não vendemos essa fruta aqui')
         break
     case 'kiwi':
-        alert("Estamos com escassez de kiwis")
+        alert('Estamos com escassez de kiwis')
         break
     case 'melancia':
-        alert("Aqui está, são 3 reais o quilo")
+        alert('Aqui está, são 3 reais o quilo')
         break
     default:
-        alert("Recarregue a página para outros resultados")
+        alert("Recarregue a página e experimento novos resultados")
 }

722ee02084d9aa4cbedc6b67d271d442c66abd6f
May_Edu <mayck22.03@hotmail.com>
2023-06-23 16:55:49 -0300

    up

---
+++ JS_loop/js/script.js
@@ -0,0 +1,35 @@
+/* console.log('WHILE')
+let tabuada = 7
+let contador = 0
+let contador3 = 0
+
+
+while (contador <= 10) {
+    console.log(tabuada + ' x ' + contador + ' = ' + tabuada * contador)
+    contador++
+}
+
+console.log('............................')
+console.log('............................')
+console.log('FOR')
+
+for (let contador2 = 0; contador2 <=10; contador2++) {
+    console.log(tabuada + ' x ' + contador2 + ' = ' + tabuada * contador2)
+}
+
+
+console.log('............................')
+console.log('............................')
+console.log('DO WHILE')
+do {
+    console.log(tabuada + ' x ' + contador3 + ' = ' + tabuada * contador3)
+    contador3++
+  } while (contador3 <= 10);
+
+*/
+let num = prompt('Digite o seu número')
+let cont = 1
+while(cont != num++){
+    console.log(cont)
+    cont++
+}

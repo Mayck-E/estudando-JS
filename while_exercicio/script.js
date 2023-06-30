ca22d45773aaeeaa81b51c1f1e521fb613a6b4aa
May_Edu <mayck22.03@hotmail.com>
2023-06-30 15:00:08 -0300

    Update script.js

--- while_exercicio/script.js
+++ while_exercicio/script.js
@@ -1,7 +1,12 @@
 
 let num = prompt('Digite o seu número')
+
 let cont = 1
 while((cont-1) != num){
+    if(num < 0 || num != Number()){
+        alert("Inválido")
+        break;
+    }
     console.log(cont)
     cont++
 }
